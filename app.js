fd51ff4bc07095db8ae99d227d06151969247df8
RodolfoKaizen <rodolfokaizen@gmail.com>
2025-03-16 11:42:40 -0300

    novo

--- app.js
+++ app.js
@@ -9,5 +9,22 @@ servidor.get('/helloword' , (req, resp) => {
 
 })
 
+servidor.get('/mensagem/boasvindas', (req, resp) => {
+    resp.send('Olá, seja bem-vindos e bem-vindas!');
+})
+
+servidor.get('/v2/mensagem/boasvindas', (req, resp) => {
+    resp.send('Que bom que você esta aqui! ;)');
+})
+
+servidor.get('/mensagem/ocupado', (req, resp) => {
+    resp.send('Estou ocupado no momento.');
+})
+
+servidor.get('/mensagem/ocupado/recado', (req, resp) => {
+    resp.send('Estou ocupado, deixe uma mensagem no emial rodolfokaizen@gmail.com');
+})
+
+
 servidor.listen(5001,  () =>  console.log(' API subida com sucesso!'));
 
