bd838face590f6165b6b9837f6a596bb63c7925c
RodolfoKaizen <rodolfokaizen@gmail.com>
2025-03-16 11:32:39 -0300

    porhora

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Oie, em breve serei uma API!!!!');
+import express, { response } from 'express';
+
+const servidor = express();
+
+
+servidor.get('/helloword' , (req, resp) => {
+    //codigo do endpoint
+    resp.send('Hello word!!! ;)');
+
+})
+
+servidor.listen(5001,  () =>  console.log(' API subida com sucesso!'));
+
